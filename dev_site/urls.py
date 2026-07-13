28c5b00cbdfc1d2039194d3fe794cd4e1628a190
Anthony <anthony.chalabis01@gmail.com>
2026-07-12 22:28:03 -0500

    Added sitemap and robots.txt

--- dev_site/urls.py
+++ dev_site/urls.py
@@ -16,8 +16,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+    path(
+        "robots.txt",
+        lambda request: HttpResponse(
+            "User-agent: *\n"
+            "Allow: /\n"
+            "Disallow: /admin/\n"
+            "\n"
+            "Sitemap: https://regionwebllc.com/sitemap.xml\n",
+            content_type="text/plain",
+        ),
+    ),
 ]
